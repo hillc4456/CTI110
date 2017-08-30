6aa5497e423b311d0fad02372eaded85a4dafac1
hillc4456 <31484912+hillc4456@users.noreply.github.com>
2017-08-30 13:25:52 -0400

    Add files via upload

---
+++ M2T1_SalesPrediction_Hill.py
@@ -0,0 +1,17 @@
+#CTI_110
+#M2T1-SalesPrediction
+#Camron Hill
+#8/30/17
+
+# Get the projected total sales.
+total_sales= float(input('Enter the project sales: '))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', format(profit, ',.2f'))
+
+
+
+       
